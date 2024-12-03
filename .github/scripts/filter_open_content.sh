81a99e4da0ed29674fe50fd0e9fc49326e1431ba
Aleksandr Duplenko <reversenant@mail.ru>
2024-12-03 09:59:50 +0200

    fix script

--- .github/scripts/filter_open_content.sh
+++ .github/scripts/filter_open_content.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-repo_dir="$1"
-
-find "$repo_dir" -type f -name "metainfo.yaml" | while read -r file; do
-  if grep -q 'ObjectId: PT' "$file"; then 
-    echo "Deleting folder: $(dirname "$file")"
-    rm -rf "$(dirname "$file")"
+find . -type f -name "metainfo.yaml" | while read -r file; do
+  if grep -q "ObjectId: PT" "$file"; then
+    dir=$(dirname "$file")
+    echo "Удаление папки: $dir"
+    rm -rf "$dir"
   fi
 done
